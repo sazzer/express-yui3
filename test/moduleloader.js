4a953e76ae50d58e0e7bb9caca517e0bc5032dc1
Graham Cox <graham@grahamcox.co.uk>
2012-05-25 21:51:54 +0100

    Able to find all modules recursively now

--- test/moduleloader.js
+++ test/moduleloader.js
@@ -7,6 +7,44 @@ var path = require("path"),
 var testdata = path.join(__dirname, "..", "testdata");
 
 describe("Module Loader", function() {
+    describe(".loadModules", function() {
+        describe("simplemodule", function() {
+            it("should find the module", function(done) {
+                moduleloader.loadModules(path.join(testdata, "simplemodule"), function(module) {
+                    should.exist(module);
+                    module.should.have.property("name", "simplemodule");
+                    module.should.have.property("version", "1.0.0");
+                    module.should.have.property("options");
+                    module.options.should.have.property("requires").with.length(2);
+                    module.options.requires.should.include("a");
+                    module.options.requires.should.include("b");
+                    module.should.have.property("files"),
+                    module.files.should.have.property("root", path.join(testdata, "simplemodule"));
+                    module.files.should.have.property("module", path.join(testdata, "simplemodule", "simplemodule.js"));
+                    
+                    done();
+                });
+            });
+        });
+        describe("testdata", function() {
+            it("should find the simplemodule module", function(done) {
+                moduleloader.loadModules(testdata, function(module) {
+                    should.exist(module);
+                    module.should.have.property("name", "simplemodule");
+                    module.should.have.property("version", "1.0.0");
+                    module.should.have.property("options");
+                    module.options.should.have.property("requires").with.length(2);
+                    module.options.requires.should.include("a");
+                    module.options.requires.should.include("b");
+                    module.should.have.property("files"),
+                    module.files.should.have.property("root", path.join(testdata, "simplemodule"));
+                    module.files.should.have.property("module", path.join(testdata, "simplemodule", "simplemodule.js"));
+                    
+                    done();
+                });
+            });
+        });
+    });
     describe(".loadModule", function() {
         describe("simplemodule", function() {
             it("should find the module", function(done) {
