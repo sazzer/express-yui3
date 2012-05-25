9a0937cc057eb0ad29ddc23d9dfa12ca705e80f9
Graham Cox <graham@grahamcox.co.uk>
2012-05-25 21:36:01 +0100

    Able to load a module from a predefined file

---
+++ test/moduleloader.js
@@ -0,0 +1,30 @@
+require("mocha");
+
+var path = require("path"),
+    should = require("should"),
+    moduleloader = require("../lib/moduleloader");
+
+var testdata = path.join(__dirname, "..", "testdata");
+
+describe("Module Loader", function() {
+    describe(".loadModule", function() {
+        describe("simplemodule", function() {
+            it("should find the module", function(done) {
+                moduleloader.loadModule(path.join(testdata, "simplemodule", "simplemodule.js"), function(module) {
+                    should.exist(module);
+                    module.should.have.property("name", "simplemodule");
+                    module.should.have.property("version", "1.0.0");
+                    module.should.have.property("options");
+                    module.options.should.have.property("requires").with.length(2);
+                    module.options.requires.should.include("a");
+                    module.options.requires.should.include("b");
+                    module.should.have.property("files"),
+                    module.files.should.have.property("root", path.join(testdata, "simplemodule"));
+                    module.files.should.have.property("module", path.join(testdata, "simplemodule", "simplemodule.js"));
+                    
+                    done();
+                });
+            });
+        });
+    });
+});
---
+++ testdata/simplemodule/simplemodule.js
@@ -0,0 +1,4 @@
+YUI.add("simplemodule", function(Y) {
+}, "1.0.0", {
+    requires: ["a", "b"]
+});
